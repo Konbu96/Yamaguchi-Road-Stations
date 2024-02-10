f7939e77083f26ecda42dbfd98b366eaf2908d43
Konbu96 <hajime1129okamoto@gmail.com>
2024-02-10 17:11:11 +0900

    add

--- main.js
+++ main.js
@@ -64,9 +64,9 @@ updateStationInfo = (station) => {
     $('#station-info-title').text(station.name);
 
     // 画像station
-    $('#station-image').attr('src',station.stationImage).alt(station.name +'の風景');
+    $('#station-image').attr('src',station.stationImage).attr('alt',station.name +'の風景');
     // 画像board
-    $('#station-board').attr('src',station.boardImage).alt(station.name +'の風景')
+    $('#station-board').attr('src',station.boardImage).attr('alt',station.name +'の風景')
 
     // 住所
     $('#station-address').text(station.address);
